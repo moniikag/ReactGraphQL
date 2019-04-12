59027fb5a8c410f750b19d5abf9773ccebd7d4a7
Monika Glier <monikaglier@gmail.com>
2019-04-12 10:55:30 +0200

    Create and get items Prisma flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,9 +1,9 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    console.log(global.dogs);
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info)
+    return item;
   }
 };
 
